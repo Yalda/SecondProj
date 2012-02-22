e92c0766010a6ac7ca9bf59f992bb17c09a8adad
Yalda <yalda.kolahdooz@gmail.com>
2012-02-22 22:27:08 +0100

    Norm is done, Pearson correlation almost done :)

--- SecondPythonProgram.py
+++ SecondPythonProgram.py
@@ -1,6 +1,7 @@
 import inputdata
 import numpy
-'''import pdb; pdb.set_trace()'''
+import scipy.stats
+#import pdb; pdb.set_trace()
 
 data = inputdata.raw_scores
 
@@ -9,7 +10,7 @@ def main():
   newElements = elements()
   print newElements.ratings
   print newElements.similarities
-
+  print newElements.pearsonCorr
 
 class elements(object):
   #I am all elements needed for recommendation :) 
@@ -47,29 +48,25 @@ class elements(object):
 	      self.ratings[i][j] = rate
 
 
-    #fill similarity norms
+    #fill similarity norms and pearson correlation
     self.similarities = numpy.zeros([len(self.people),len(self.people)])    
+    self.pearsonCorr = numpy.zeros([len(self.people),len(self.people)])    
     i=0
     for person in self.people:
       j=0
       for preson in self.people:
         self.similarities[i][j] = self.norm_finder(self.ratings[i],self.ratings[j])
+	self.pearsonCorr[i][j] = self.pearson_finder(self.ratings[i],self.ratings[j])
 	j+= 1
       i+= 1
 
-
-
-
-
-#------------------------------------------------------------------
-
-   
+  #------------------------- norm --------------------------------------------------  
   def norm_finder(self,v1,v2):
     return numpy.linalg.norm(v1-v2)
 
-
-
-
+  #------------------------- pearson correlation------------------------------------  
+  def pearson_finder(self,v1,v2):
+    return scipy.stats.pearsonr(v1,v2)
       
 
 main()
